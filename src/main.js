233721bda122093ce079d5ce0717d971be3aadda
Ian Camacho <iancm.mejia@gmail.com>
2021-02-06 22:17:48 -0600

    Creacion de una pagina para ocnsultar datos de jugadores de Diablo. CUrso de Platzi

--- src/main.js
+++ src/main.js
@@ -1,12 +1,32 @@
+// Paquetes de npm
 import Vue from 'vue'
+// BootstrapVue
+import './plugins/BootstrapVue'
+// Vue Font-Awesome
+import './plugins/FontAwesome'
+// Custom directives
+import './directives'
+// Archivos locales de nuestra App
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
-Vue.config.productionTip = false
+// CSS global
+import './assets/css/main.styl'
 
+// Configuración extra
+Vue.config.productionTip = false
+// Instancia principal de Vue
 new Vue({
   router,
   store,
+  methods: {
+    init () {
+      store.dispatch('oauth/getToken', null, { root: true })
+    }
+  },
+  created () {
+    this.init()
+  },
   render: h => h(App)
 }).$mount('#app')
